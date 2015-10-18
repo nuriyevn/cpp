d30c3ed4bd95c4cef89b4e4e655b367b0e432f9c
nuriyevn <nuriyevn@gmail.com>
2015-10-18 14:15:36 +0300

    Commit for Tatiana

---
+++ a2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+int main() 
+{
+int a,digits[10],count,i,t;
+scanf("%d",&a);
+count=0;
+while (a!=0) 
+{
+t=a%10;
+digits[count]=t;
+a=a/10;
+count=count+1;
+}
+
+for (i=count-1; i>=0;i--)
+printf("%d ",digits[i]);
+
+}
---
+++ prj5.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+
+int main()
+{
+	int a, b, c;
+	// reading two numbers
+	scanf("%d%d", &a, &b);
+	// calculating
+	c = a+b;
+	// writing result
+	printf("%d",c);
+}
+
+
