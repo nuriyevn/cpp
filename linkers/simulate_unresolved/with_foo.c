4694e982a2ae2cd34e65adf4589ac158f54cc1b4
nuriyevn <nuriyevn@gmail.com>
2015-11-19 15:47:50 +0200

    Linkers: class mangling

--- linkers/simulate_unresolved/with_foo.c
+++ linkers/simulate_unresolved/with_foo.c
@@ -2,6 +2,13 @@ int foo(int a, int b)
 {
 }
 
+class MyClass
+{
+   public:
+      void doSmth(struct baz* b)
+      {
+      }
+};
 
 struct baz
 {
@@ -10,8 +17,15 @@ struct baz
 struct baz* bar(struct baz i, struct baz *pi, struct baz *pi2)
 {
 }
+void func(void)
+{
+}
 
 int main()
 {
    foo(3, 4);
+   MyClass c;
+   c.doSmth(new baz());
+      
+
 }
